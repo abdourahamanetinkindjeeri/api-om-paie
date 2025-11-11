6c9e910aec4d0ce622d68e0d6fcc97e423a6c9f8
tinkin <tinkin@tinkin.home>
2025-11-11 09:36:30 +0000

    Corriger les cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:8080',
+        'http://127.0.0.1:8000',
+        'https://tinkin-transfer.onrender.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
